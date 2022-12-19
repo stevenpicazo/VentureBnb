04d1e83b1aa85f35c09961e360e43a5c73ba268f
stevenpicazo <stevenpicazo01@gmail.com>
2022-12-19 15:33:01 -0800

    created spots seeder

--- backend/db/seeders/20221219215924-demo-spots.js
+++ backend/db/seeders/20221219215924-demo-spots.js
@@ -12,35 +12,46 @@ module.exports = {
     options.tableName = 'Spots';
     return queryInterface.bulkInsert(options, [
       {
-        firstName: 'userFirstName1',
-        lastName: 'userLastName1',
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        ownerId: 1,
+        address: '123 AddressOne',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'United States of America',
+        lat: 37.7645358,
+        lng: -122.4730327,
+        name: 'demo-name1',
+        description: 'Dream Land1',
+        price: 101.1
       },
       {
-        firstName: 'userFirstName2',
-        lastName: 'userLastName2',
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        ownerId: 2,
+        address: '123 AddressTwo',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'United States of America',
+        lat: 47.7645358,
+        lng: -223.4730327,
+        name: 'demo-name2',
+        description: 'Dream Land2',
+        price: 102.2
       },
       {
-        firstName: 'userFirstName3',
-        lastName: 'userLastName3',
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        ownerId: 3,
+        address: '123 AddressThree',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'United States of America',
+        lat: 57.7645358,
+        lng: -334.4730327,
+        name: 'demo-name2',
+        description: 'Dream Land3',
+        price: 103.3
       }
     ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Spots';
+    return queryInterface.bulkDelete(options, {});
   }
 };
