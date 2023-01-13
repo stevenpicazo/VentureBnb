e3e620910359ada0c7c33409e3d011fb3f67a51e
stevenpicazo <stevenpicazo01@gmail.com>
2023-01-12 21:40:58 -0800

    SignupFormPage compenent created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-// frontend/src/App.js
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -16,6 +16,9 @@ function App() {
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,6 +17,13 @@ const removeUser = () => {
   };
 };
 
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
@@ -31,12 +38,24 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
-export const restoreUser = () => async dispatch => {
-    const response = await csrfFetch('/api/session');
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
   };
+
+
 
 const initialState = { user: null };
 
