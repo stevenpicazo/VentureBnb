7e9ca566594723d8a5f5f119cc7a25abdf8c1da5
stevenpicazo <stevenpicazo01@gmail.com>
2023-01-12 21:10:04 -0800

    loaded session reducer to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-// import * as sessionActions from './store/session';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-  // window.sessionActions = sessionActions;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
