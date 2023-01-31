08b36cb393a6a78c1e2c76b1e98ac0fb7481b601
stevenpicazo <stevenpicazo01@gmail.com>
2023-01-31 09:38:16 -0800

    Spot details component organized for bookings implementation

--- frontend/src/components/SpotsDetails/SpotDetails.js
+++ frontend/src/components/SpotsDetails/SpotDetails.js
@@ -106,155 +106,159 @@ function SpotDetails() {
                         </div>
                     </div>
                 ))}
+                
+                <div className="description-bookings">
 
-                <section className="bookings-section">
-                    <div className="bookings-container">
-                        <div className="bookings-wrapper">
-                            <div className="bookings">
-                                TEST
+                    <div className="spot-description-container">
+                        <div className="info">
+
+                            <div className="listing-info-container">
+                                <div className="hosting-info">
+                                    Entire home hosted by {spot.Owner.firstName}
+                                </div>
+                                <div className="hosting-border"></div>
+                                <div className="listing-info">
+                                    6 guests · 4 bedrooms · 7 beds · 2 baths
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-            </section>
-            <div className="spot-description-container">
-                <div className="info">
-
-                    <div className="listing-info-container">
-                        <div className="hosting-info">
-                            Entire home hosted by {spot.Owner.firstName}
-                        </div>
-                        <div className="hosting-border"></div>
-                        <div className="listing-info">
-                            6 guests · 4 bedrooms · 7 beds · 2 baths
-                        </div>
-                    </div>
-
-                    {/* <i class="fa-regular fa-calendar"></i> */}
-                    <div className="hosting-info-container">
-                        <div className="check-in">
-                            <i className="fa-regular fa-calendar"></i>Self check-in
-                        </div>
-                        <span className="check-in-info">Check in yourself through key pad.</span>
-                        <div className="super-host">
-                            <i className="fa-regular fa-circle-user"></i>
-                            {spot.Owner.firstName} is a Superhost
-                        </div>
-                        <span className="super-host-info">
-                            Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.
-                        </span>
-
-                        <div className="work-space">
-                            <i className="fa-regular fa-pen-to-square"></i>Designated work space
-                        </div>
-                    </div>
-                    <div className="air-cover-container">
-                        <div className="air-cover-title">
-                            <h2 className="cover-air"></h2>
-                        </div>
-                        <div className="air-cover-desc">
-                            Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <main className="reviews-wrapper">
-                <div className="num-reviews">
-                    {!spot.numReviews ? 'No Reviews' : `★ ${rating} · ${spot.numReviews} reviews`}
-
-                </div>
-                <>
-                    <div className="user-reviews-container">
-                        {reviewsObj && Object.values(reviewsObj).map(review => (
-                            <div className="reviews-wrap">
-                                <div className="user-ratings">
+                            {/* <i class="fa-regular fa-calendar"></i> */}
+                            <div className="hosting-info-container">
+                                <div className="check-in">
+                                    <i className="fa-regular fa-calendar"></i>Self check-in
+                                </div>
+                                <span className="check-in-info">Check in yourself through key pad.</span>
+                                <div className="super-host">
                                     <i className="fa-regular fa-circle-user"></i>
-                                    {review.User.firstName}
+                                    {spot.Owner.firstName} is a Superhost
                                 </div>
-                                <div className="user-reviews">
-                                    {review.review}
+                                <span className="super-host-info">
+                                    Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.
+                                </span>
+
+                                <div className="work-space">
+                                    <i className="fa-regular fa-pen-to-square"></i>Designated work space
                                 </div>
                             </div>
+                            <div className="air-cover-container">
+                                <div className="air-cover-title">
+                                    <h2 className="cover-air"></h2>
+                                </div>
+                                <div className="air-cover-desc">
+                                    Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.
 
-                        ))}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    {reviewsObj && Object.values(reviewsObj).map(review => (
-                        sessionUser && (
-                            <div review={review} className={"reviewInfo"} key={`review-${review.id}`}>
-                                {sessionUser.id === review.userId && (
-                                    <button
-                                        className="delete-review-button"
-                                        onClick={(e) => deleteReview(e, review.id)}>
-                                        Delete review
-                                    </button>
-                                )}
-                            </div>
-                        )))}
-                </>
-            </main>
 
-
-            <div>
-                {sessionUser && !booleanFlag && sessionUser.id !== spot.ownerId ?
-                    <>
-                        <button
-                            className="create-review-button"
-                            onClick={() => { setIsFormOpen(!isFormOpen) }}>
-                            Create Review
-                        </button>
-
-                    </>
-                    : null
-
-                }
-            </div>
-
-
-
-            {isFormOpen && sessionUser?.id !== spot.ownerId && booleanFlag === false ? (
-                <>
-                    <form className="reviews-form" onSubmit={handleSubmit}>
-                        <ul className="ul-errors">
-                            {validationErrors.map(error => (
-                                <div
-                                    className="errors"
-                                    key={error}>{error}
+                    <section className="bookings-section">
+                        <div className="bookings-container">
+                            <div className="bookings-wrapper">
+                                <div className="bookings">
+                                    TEST
                                 </div>
+                            </div>
+                        </div>
+
+                    </section>
+
+                </div>
+
+                <main className="reviews-wrapper">
+                    <div className="num-reviews">
+                        {!spot.numReviews ? 'No Reviews' : `★ ${rating} · ${spot.numReviews} reviews`}
+
+                    </div>
+                    <>
+                        <div className="user-reviews-container">
+                            {reviewsObj && Object.values(reviewsObj).map(review => (
+                                <div className="reviews-wrap">
+                                    <div className="user-ratings">
+                                        <i className="fa-regular fa-circle-user"></i>
+                                        {review.User.firstName}
+                                    </div>
+                                    <div className="user-reviews">
+                                        {review.review}
+                                    </div>
+                                </div>
+
                             ))}
-                        </ul>
-                        <div>
-                            <label htmlFor='reviews'>Review:</label>
-                            <input
-                                className='reviews'
-                                type='text'
-                                onChange={e => setReview(e.target.value)}
-                                value={review}
-                                placeholder='Share your thoughts about your stay!'
-                                required
-                            />
                         </div>
-                        <div>
-                            <label htmlFor='stars'>Stars:</label>
-                            <input
-                                className='stars'
-                                type='number'
-                                min={1}
-                                max={5}
-                                onChange={e => setStars(e.target.value)}
-                                value={stars}
-                                placeholder='Rate your stay!'
-                                required
-                            />
-                        </div>
-                        <button className='submit-review-button'>Submit</button>
-                    </form>
-                </>
-            ) : (
-                (null))}
-        </div>
+                        {reviewsObj && Object.values(reviewsObj).map(review => (
+                            sessionUser && (
+                                <div review={review} className={"reviewInfo"} key={`review-${review.id}`}>
+                                    {sessionUser.id === review.userId && (
+                                        <button
+                                            className="delete-review-button"
+                                            onClick={(e) => deleteReview(e, review.id)}>
+                                            Delete review
+                                        </button>
+                                    )}
+                                </div>
+                            )))}
+                    </>
+                </main>
+
+
+                <div>
+                    {sessionUser && !booleanFlag && sessionUser.id !== spot.ownerId ?
+                        <>
+                            <button
+                                className="create-review-button"
+                                onClick={() => { setIsFormOpen(!isFormOpen) }}>
+                                Create Review
+                            </button>
+
+                        </>
+                        : null
+
+                    }
+                </div>
+
+
+
+                {isFormOpen && sessionUser?.id !== spot.ownerId && booleanFlag === false ? (
+                    <>
+                        <form className="reviews-form" onSubmit={handleSubmit}>
+                            <ul className="ul-errors">
+                                {validationErrors.map(error => (
+                                    <div
+                                        className="errors"
+                                        key={error}>{error}
+                                    </div>
+                                ))}
+                            </ul>
+                            <div>
+                                <label htmlFor='reviews'>Review:</label>
+                                <input
+                                    className='reviews'
+                                    type='text'
+                                    onChange={e => setReview(e.target.value)}
+                                    value={review}
+                                    placeholder='Share your thoughts about your stay!'
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='stars'>Stars:</label>
+                                <input
+                                    className='stars'
+                                    type='number'
+                                    min={1}
+                                    max={5}
+                                    onChange={e => setStars(e.target.value)}
+                                    value={stars}
+                                    placeholder='Rate your stay!'
+                                    required
+                                />
+                            </div>
+                            <button className='submit-review-button'>Submit</button>
+                        </form>
+                    </>
+                ) : (
+                    (null))}
+            </div>
 
         </div >
     )
