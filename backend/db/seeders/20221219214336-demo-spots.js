0f29dabbec92d9ac220743a37c04e5edfe7b1fac
stevenpicazo <stevenpicazo01@gmail.com>
2023-02-04 16:24:14 -0800

    Spot description seeders updated to oblige with corresponding validators

--- backend/db/seeders/20221219214336-demo-spots.js
+++ backend/db/seeders/20221219214336-demo-spots.js
@@ -20,7 +20,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'One Mile High Home',
-        description: 'A spacious country house surrounded by lush green gardens, ideal for groups of friends or large families. The house also has a large private pool, a hot tub, and plenty of outdoor seating areas, perfect for relaxing and enjoying the beautiful countryside views.',
+        description: 'A spacious country house surrounded by lush green gardens, ideal for groups of friends. The house also has a large private pool and plenty of outdoor seating areas, perfect for relaxing and enjoying the beautiful countryside views.',
         price: 268
       },
       {
@@ -44,7 +44,7 @@ module.exports = {
         lat: 37.7645358,
         lng: -122.4730327,
         name: 'Lake Side Views, Game Room',
-        description: 'Experience the ultimate lakeside getaway in this cozy cabin with breathtaking views of the lake. The open-plan living area features a comfortable fireplace, large windows, and plenty of natural light, allowing you to enjoy the stunning lake views from the comfort of your own home. ',
+        description: 'Experience the ultimate lakeside getaway in this cozy cabin with breathtaking views of the lake. The open-plan living area allows you to enjoy the stunning lake views from the comfort of your own home.',
         price: 1000
       },
       {
@@ -56,7 +56,7 @@ module.exports = {
         lat: 47.7645358,
         lng: -223.4730327,
         name: 'Mid-Century Ensenada Retreat',
-        description: 'Savor the laid-back beach lifestyle in this stunning home with breathtaking views of the ocean in Ensenada, Mexico. The open-plan living area features floor-to-ceiling windows, comfortable furnishings, and plenty of natural light, offering panoramic views of the ocean from every angle.',
+        description: 'Savor the laid-back beach lifestyle in this stunning home with breathtaking views of the ocean in Ensenada, Mexico. The open-plan living area features plenty of natural light, offering panoramic views of the ocean from every angle.',
         price: 550
       },
       {
@@ -80,7 +80,7 @@ module.exports = {
         lat: 37.7645358,
         lng: -122.4730327,
         name: 'Rosa Estate',
-        description: 'A charming cottage in the heart of the city, perfect for couples or small families. This lovely home features a comfortable living room with a plush sofa and a flat-screen TV, a fully equipped kitchen with all the essentials. The cottage also boasts a small private garden where guests can relax and enjoy the beautiful city views.',
+        description: 'A charming cottage in the heart of the city, perfect for small families. This lovely home features a comfortable living room with a plush sofa and a flat-screen TV. The cottage also boasts a small private garden where guests can relax and enjoy.',
         price: 300
       },
       {
@@ -92,7 +92,7 @@ module.exports = {
         lat: 47.7645358,
         lng: -223.4730327,
         name: 'Luxury Lodge',
-        description: 'Indulge in the ultimate luxury getaway at this stunning lodge, surrounded by breathtaking natural scenery. The spacious and elegantly designed living area features high ceilings, comfortable furnishings, and an abundance of natural light, creating a warm and inviting atmosphere.',
+        description: 'Indulge in the ultimate luxury getaway at this stunning lodge, surrounded by breathtaking natural scenery. The spacious and elegantly designed living area features high ceilings and an abundance of natural light, creating a warm and inviting atmosphere.',
         price: 680
       },
       {
@@ -104,7 +104,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Luxurious Spanish Villa',
-        description: 'Escape to this enchanting Spanish villa, nestled amidst lush greenery and breathtaking natural scenery. The fully equipped gourmet kitchen is perfect for entertaining guests, while the comfortable bedrooms and stylish bathrooms provide a restful retreat. Take a dip in the pool, lounge on the terrace, and explore the nearby nature trails in this tranquil and serene Spanish villa escape.',
+        description: 'Escape to this enchanting Spanish villa, nestled amidst lush greenery and breathtaking natural scenery. Take a dip in the pool, lounge on the terrace, and explore the nearby nature trails in this tranquil and serene Spanish villa escape.',
         price: 735
       },
       {
@@ -116,7 +116,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Cozy Luxorious Loft',
-        description: 'Experience the ultimate jungle getaway in this unique and spacious loft, surrounded by lush vegetation and towering trees. The open-plan living area features high ceilings, plenty of natural light, and a comfortable lounge area, offering a breathtaking view of the surrounding jungle.',
+        description: 'Experience the ultimate jungle getaway in this unique and spacious loft, surrounded by lush vegetation and towering trees. The open-plan living area features a comfortable lounge area, offering a breathtaking view of the surrounding jungle.',
         price: 300
       },
       {
@@ -128,7 +128,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Cliffside Estate',
-        description: 'Entertain in style at this chic and spacious home, featuring a stunning pool, spacious outdoor area, and fully equipped indoor kitchen. The sleek and modern living area features comfortable furnishings, clean lines, and an abundance of natural light. The fully equipped kitchen is perfect for hosting guests, while the stylish bedrooms and luxurious bathrooms provide a comfortable escape. Enjoy al fresco dining by the pool, lounge on the sun loungers, and entertain guests in this beautiful and contemporary setting.',
+        description: 'Entertain in style at this chic and spacious home, featuring a stunning pool, spacious outdoor area, and fully equipped indoor kitchen. The sleek and modern living area features comfortable furnishings, clean lines, and an abundance of natural light.',
         price: 450
       },
       {
@@ -140,7 +140,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Modern Luxury Retreat',
-        description: "Embrace the ultimate lake lifestyle in this stunning and modern house, directly connected to the water's edge. The open-plan living area features floor-to-ceiling windows, comfortable furnishings, and an abundance of natural light, offering breathtaking views of the lake from every angle.",
+        description: "Embrace the ultimate lake lifestyle in this stunning and modern house, directly connected to the water's edge. The open-plan living area features an abundance of natural light, offering breathtaking views of the lake from every angle.",
         price: 1250
       },
       {
@@ -152,7 +152,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Hill Side Estate',
-        description: "Escape to the mountains in this contemporary and luxurious hillside home, offering breathtaking views of the snow-capped peaks. The spacious and elegantly designed living area features floor-to-ceiling windows, comfortable furnishings, and an abundance of natural light, offering stunning views of the surrounding landscape.",
+        description: "Escape to the mountains in this luxurious hillside home, offering breathtaking views of the snow-capped peaks. The spacious and elegantly designed living area features comfortable furnishings offering stunning views of the surrounding landscape.",
         price: 300
       },
       {
@@ -164,7 +164,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Noble Estate',
-        description: 'Nestled among the trees, this charming and cozy cabin offers a peaceful retreat in the heart of the woods. The warm and inviting living area features a crackling fireplace, comfortable furnishings, and plenty of natural light. The fully equipped kitchen is perfect for whipping up meals, while the comfortable bedrooms and quaint bathrooms provide a restful retreat.',
+        description: 'Nestled among the trees, this charming and cozy cabin offers a peaceful retreat in the heart of the woods. The fully equipped kitchen is perfect for whipping up meals, while the comfortable bedrooms and quaint bathrooms provide a restful retreat.',
         price: 300
       },
       {
@@ -188,7 +188,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Herald Cabin',
-        description: 'Escape to this cozy and charming cabin in the heart of Big Bear. Surrounded by stunning mountain scenery, this cabin offers a peaceful retreat from the hustle and bustle of city life. The warm and inviting living area features a comfortable fireplace, soft lighting, and rustic décor, creating a welcoming atmosphere.',
+        description: 'Escape to this cozy and charming cabin in the heart of Big Bear. This cabin offers a peaceful retreat from the hustle and bustle of city life. The living area features a comfortable, rustic décor, creating a welcoming atmosphere.',
         price: 300
       },
       {
@@ -200,7 +200,7 @@ module.exports = {
         lat: 57.7645358,
         lng: -334.4730327,
         name: 'Noble Estate',
-        description: 'Elevate your vacation experience in this sleek and contemporary home, featuring a stunning infinity pool overlooking the city skyline. The fully equipped kitchen is perfect for entertaining guests, while the spacious bedrooms and stylish bathrooms provide a luxurious retreat. Take a dip in the pool, relax on the sun loungers, and enjoy the beauty of the city from this modern oasis.',
+        description: 'Elevate your vacation experience in this sleek and contemporary home, featuring a stunning infinity pool overlooking the city skyline. The fully equipped spacious bedrooms have stylish bathrooms and all provide for a luxurious retreat.',
         price: 300
       },
 
