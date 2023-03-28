30b552ece4cb4d4b63fd2aa05d85d41d12ca25dc
stevenpicazo <stevenpicazo01@gmail.com>
2023-03-28 14:54:42 -0700

    search bars bugs fixed

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
+import { readThunk } from "../../store/spots";
 import './SearchBar.css'
 
 const SearchBar = () => {
     const history = useHistory();
+    const dispatch = useDispatch();
     const [search, setSearch] = useState("");
     const [searchResults, setSearchResults] = useState([]);
     const spots = useSelector(state => state.spots)
@@ -13,16 +15,20 @@ const SearchBar = () => {
     const handleSearch = (e) => {
         const inputValue = e.target.value;
         setSearch(inputValue);
-        const results = allSpots.filter(spot =>
-            spot.city.toLowerCase().includes(inputValue.toLowerCase()) ||
-            spot.state.toLowerCase().includes(inputValue.toLowerCase()) ||
-            spot.country.toLowerCase().includes(inputValue.toLowerCase()))
+        const results = allSpots?.filter(spot =>
+            spot?.city.toLowerCase().includes(inputValue.toLowerCase()) ||
+            spot?.state.toLowerCase().includes(inputValue.toLowerCase()) ||
+            spot?.country.toLowerCase().includes(inputValue.toLowerCase()))
         if (inputValue === "") {
             setSearchResults([])
         } else {
             setSearchResults(results)
         }
     }
+
+    useEffect(() => {
+      dispatch(readThunk());
+    }, [dispatch]);
 
     return (
         <div className="search-wrapper">
@@ -49,18 +55,23 @@ const SearchBar = () => {
                 {searchResults.length > 0 && (
                     <div className="search-results-container">
                         {searchResults?.slice(0, 7).map((spot) => (
-                            <div className="search-result"
-                                onClick={() => {
-                                    setSearchResults([]);
-                                    setSearch("");
-                                    history.push(`/spots/${spot.id}`);
-                                }}                               >
-                                <img className="search-result-image" src={spot.images[0].url} />
-                                <div className="search-result-info">
-                                    <div className="search-result-name">{spot.name}</div>
-                                    <div className="search-result-location">in {spot.city}, {spot.state}</div>
-                                </div>
-                            </div>
+                            <>
+                                {console.log('spot', spot)}
+                                {spot && spot?.images && spot?.images[0] &&
+                                    <div className="search-result"
+                                        onClick={() => {
+                                            setSearchResults([]);
+                                            setSearch("");
+                                            history.push(`/spots/${spot.id}`);
+                                        }}                               >
+                                        <img className="search-result-image" src={spot?.images[0].url} />
+                                        <div className="search-result-info">
+                                            <div className="search-result-name">{spot?.name}</div>
+                                            <div className="search-result-location">in {spot?.city}, {spot?.state}</div>
+                                        </div>
+                                    </div>
+                                }
+                            </>
                         ))}
                     </div>
                 )}
